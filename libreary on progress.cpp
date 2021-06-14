ac30953005d5174aa3ecaa266128845f523a9953
Uttam <uttamcseau@gmail.com>
2021-06-14 23:38:29 +0530

    simple project

---
+++ libreary on progress.cpp
@@ -0,0 +1,314 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bookissu()
+{
+	int sem;
+	int sub;
+	
+	cout<<"please enter your semester "<<endl;
+	cin>>sem;
+		
+
+	
+	switch(sem)
+	{
+		case 1:
+			cout<<"1 --physics"<<endl<<"2 --mathematics"<<endl<<"3 --Bee"<<endl;;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"physics has been  issued"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"mathematics has been issued"<<endl;
+			}
+				else if(sub==3)
+			{
+				cout<<"bee has been issued"<<endl;
+			}
+				else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+				case 2:
+			cout<<"1 --mathematics-II"<<endl<<"2 --chemistry"<<endl<<"3 --c programming"<<endl<<"4 --pc"<<endl;;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"mathematics-II has been issued"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"chemistry has been issued"<<endl;
+			}
+			else if(sub==3)
+			{
+				cout<<"c-programming has been issued"<<endl;
+			}
+			else if(sub==4)
+			{
+				cout<<"pc has been issued"<<endl;
+			}
+			else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+				case 3:
+			cout<<"1 --ds"<<endl<<"2 --dld"<<endl<<"3 --dstl"<<endl<<"4 --tc"<<endl<<"5 --energy science"<<endl<<"5 --evs"<<endl;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"ds has been issued"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"dld has been issued"<<endl;
+			}
+			else if(sub==3)
+			{
+				cout<<"dstl has been issued"<<endl;
+			}
+			else if(sub==4)
+			{
+				cout<<"tc has been issued"<<endl;
+			}
+				else if(sub==5)
+			{
+				cout<<"energy science has been issued"<<endl;
+			}
+			else if(sub==6)
+			{
+				cout<<"evs has been issued"<<endl;
+			}
+			else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+				case 4:
+			cout<<"1 --c++"<<endl<<"2 --toc"<<endl<<"3 --os"<<endl<<"4 --coa"<<endl<<"5 --syber securety"<<endl<<"5 --hbd"<<endl;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"c++ has been issued"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"toc has been issued"<<endl;
+			}
+			else if(sub==3)
+			{
+				cout<<"os has been issued"<<endl;
+			}
+			else if(sub==4)
+			{
+				cout<<"coa has been issued"<<endl;
+			}
+				else if(sub==5)
+			{
+				cout<<"syber securty has been issued"<<endl;
+			}
+			else if(sub==6)
+			{
+				cout<<"hbd has been issued"<<endl;
+			}
+			else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+			default:
+				cout<<"sorry.........";
+				break;
+	}
+}
+
+
+
+bookreturn()
+{
+	int sem;
+	int sub;
+	
+	cout<<"please enter your semester "<<endl;
+	cin>>sem;
+		
+
+	
+	switch(sem)
+	{
+		case 1:
+			cout<<"1 --physics"<<endl<<"2 --mathematics"<<endl<<"3 --Bee"<<endl;;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"physics has been  returned succesfully"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"mathematics has been returned succesfully"<<endl;
+			}
+				else if(sub==3)
+			{
+				cout<<"bee has been returned succesfully"<<endl;
+				
+			}
+				else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+				case 2:
+			cout<<"1 --mathematics-II"<<endl<<"2 --chemistry"<<endl<<"3 --c programming"<<endl<<"4 --pc"<<endl;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"mathematics-II has been returned succesfully"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"chemistry has been returned succesfully"<<endl;
+			}
+			else if(sub==3)
+			{
+				cout<<"c-programming has been returned succesfully"<<endl;
+			}
+			else if(sub==4)
+			{
+				cout<<"pc has been returned succesfully"<<endl;
+			}
+			else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+				case 3:
+			cout<<"1 --ds"<<endl<<"2 --dld"<<endl<<"3 --dstl"<<endl<<"4 --tc"<<endl<<"5 --energy science"<<endl<<"5 --evs"<<endl;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"ds has been returned succesfully"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"dld has been returned succesfully"<<endl;
+			}
+			else if(sub==3)
+			{
+				cout<<"dstl has been returned succesfully"<<endl;
+			}
+			else if(sub==4)
+			{
+				cout<<"tc has been returned succesfully"<<endl;
+			}
+				else if(sub==5)
+			{
+				cout<<"energy science has been returned succesfully"<<endl;
+			}
+			else if(sub==6)
+			{
+				cout<<"evs has been returned succesfully"<<endl;
+			}
+			else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+				case 4:
+			cout<<"1 --c++"<<endl<<"2 --toc"<<endl<<"3 --os"<<endl<<"4 --coa"<<endl<<"5 --syber securety"<<endl<<"5 --hbd"<<endl;
+				cout<<"please enter your subject "<<endl;
+				cin>>sub;
+			if(sub==1)
+			{
+				cout<<"c++ has been returned succesfully"<<endl;
+			}
+			else if(sub==2)
+			{
+				cout<<"toc has been returned succesfully"<<endl;
+			}
+			else if(sub==3)
+			{
+				cout<<"os has been returned succesfully"<<endl;
+			}
+			else if(sub==4)
+			{
+				cout<<"coa has been returned succesfully"<<endl;
+			}
+				else if(sub==5)
+			{
+				cout<<"syber securty has been returned succesfully"<<endl;
+			}
+			else if(sub==6)
+			{
+				cout<<"hbd has been returned succesfully"<<endl;
+			}
+			else 
+			{
+				cout<<"invailid choice"<<endl;
+			}
+			break;
+			default:
+				cout<<"sorry.........";
+				break;
+	}
+}
+
+createaccount()
+{
+	string uname;
+	string password;
+
+		cout<<"please enter your user name :"<<endl; 
+		cin>>uname;
+		cout<<"please enter your password :"<<endl; 
+		cin>>password;
+		
+		cout<<"your account has been created succsefully"<<endl<<"username : "<<uname<<endl<<"password :"<<password<<endl;	
+}
+
+int main()
+{
+	while(1)
+	{
+	
+	int a;
+cout<<"Hello sir your welcome in my libraery"<<endl;
+cout<<"please enter your choice "<<endl;
+cout<<"1 --book issu"<<endl<<"2 --book return"<<endl<<"3 --create account"<<endl;
+cin>>a;
+
+
+if(a==1)
+	{
+	bookissu();
+	}
+else if(a==2)
+	{
+	bookreturn();
+	}
+else if	(a==3)
+{
+	createaccount();
+}
+	
+else
+{
+	cout<<"sorry invailid choice"<<endl;
+	}	
+	
+cin>>a;
+}
+return 0;
+}
---
+++ librery using cons.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class bookissu
+{
+
+	
+	public : 
+	
+	funbookissu(int sem ,int sub);
+
+};
+bookissu :: funbookissu(int sem,int sub)
+{
+	
+	switch(sem)
+	{
+		case 1:
+			if(sub==1)
+			{
+				cout<<"ds"<<endl;
+			
+			}
+			else if(sub==2)
+			{
+				cout<<"dld"<<endl;
+				
+			}
+			else
+			{
+				cout<<"error sem 1"<<endl;
+			}
+			
+		case 2 :
+			if(sub==1)
+			{
+				cout<<"dstl"<<endl;
+			
+			}
+			else if(sub==2)
+			{
+				cout<<"dbms"<<endl;
+				
+			}
+			else
+			{
+				cout<<"error"<<endl;
+			}
+			break;
+		default :
+				{
+					cout<<"sorrrr"<<endl;
+				}
+	}
+}
+
+
+int main()
+{
+
+bookissu cse;
+int sub,sem;
+cout<<"sem"<<endl;
+cin>>sem;
+
+cout<<"sub"<<endl;
+cin>>sub;
+
+cse.funbookissu(sub,sem);
+
+
+return 0;
+}
